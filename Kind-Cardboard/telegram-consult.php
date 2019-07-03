fa837c4ab41f99df8e6cccd2a6210e934ac7a88c
AronAP <artem.prydybailo@gmail.com>
2019-07-03 20:07:16 +0300

    Tried to fix 405 Not Allowed ERROR

--- Kind-Cardboard/telegram-consult.php
+++ Kind-Cardboard/telegram-consult.php
@@ -1,44 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-/* https://api.telegram.org/bot881409762:AAFN6EE9LjERffoYMZqQ2VwCcj8DJX8OByA/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+	<head>
+		<meta charset="UTF-8">
+		<title>Форма</title>
+	</head>
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$length = $_POST['product_length'];
-$width = $_POST['product_width'];
-$height = $_POST['product_height'];
-$number = $_POST['product_number'];
-$token = "881409762:AAFN6EE9LjERffoYMZqQ2VwCcj8DJX8OByA";
-$chat_id = "-388235252";
-$separator = "";
+	<body>
 
-if($phone == "") {
-	$phone = "НЕИЗВЕСТЕН";
-} else {
-	$phone = urlencode("+").$phone;
-}
+		<?php
+		error_reporting(-1);
+		header('Content-Type: text/html; charset=utf-8');
 
-$arr = array(
-  "Имя: " => $name,
-	"Телефон: " => $phone,
-	"_____________" => $separator,
-	"Длина изделия (м): " => $length . "м",
-	"Ширина изделия (м): " => $width . "м",
-	"Высота изделия (м): " => $height . "м",
-	"Количество изделий (штук): " => $number . " штук"
-);
+		/* https://api.telegram.org/bot881409762:AAFN6EE9LjERffoYMZqQ2VwCcj8DJX8OByA/getUpdates,
+		где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
-foreach($arr as $key => $value) {
-	$txt .= "<b>".$key."</b> ".$value."%0A";
-};
+		$name = $_POST['user_name'];
+		$phone = $_POST['user_phone'];
+		$length = $_POST['product_length'];
+		$width = $_POST['product_width'];
+		$height = $_POST['product_height'];
+		$number = $_POST['product_number'];
+		$token = "881409762:AAFN6EE9LjERffoYMZqQ2VwCcj8DJX8OByA";
+		$chat_id = "-388235252";
+		$separator = "";
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+		if($phone == "") {
+			$phone = "НЕИЗВЕСТЕН";
+		} else {
+			$phone = urlencode("+").$phone;
+		}
 
-if ($sendToTelegram) {
-	header('Location: thank-you.php');
-} else {
-	header('Location: error.html');
-}
+		$arr = array(
+			"Имя: " => $name,
+			"Телефон: " => $phone,
+			"_____________" => $separator,
+			"Длина изделия (м): " => $length . "м",
+			"Ширина изделия (м): " => $width . "м",
+			"Высота изделия (м): " => $height . "м",
+			"Количество изделий (штук): " => $number . " штук"
+		);
 
-?>
+		foreach($arr as $key => $value) {
+			$txt .= "<b>".$key."</b> ".$value."%0A";
+		};
+
+		$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+		if ($sendToTelegram) {
+			header('Location: thank-you.php');
+		} else {
+			header('Location: error.html');
+		}
+
+		?>
+
+	</body>
+</html>
